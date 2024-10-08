ad421edead3a6a40ffc2d591eb785c135c11538f
Roman <77562623+moonsoonredt@users.noreply.github.com>
2024-10-08 11:23:40 +0300

    пока все

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+
+    return (
+        <header>
+            <h1>Заголовок</h1>
+            <h2>Подзаголовок</h2>
+            <p>Текст</p>
+        </header>
+    )
+}
